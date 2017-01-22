97a48c8fd1da055bfa72e6dd606f6dae97081778
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2017-01-22 18:08:00 +0100

    Small test project to play with dotnet core.

---
+++ src/PayTheBill/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var users = new List<string>();
+
+            while (true)
+            {
+                DisplayMenu();
+
+                var choice = Console.ReadLine();
+
+                users.Add(choice);
+
+                Console.WriteLine("You chose '{0}'", choice);
+                
+                Console.WriteLine("Full list of users:");
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nMenu");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static int Add(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
---
+++ test/PayTheBill.Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var actual = ConsoleApplication.Program.Add(2, 3);
+
+            Assert.True(actual == 5);
+        }
+    }
+}
